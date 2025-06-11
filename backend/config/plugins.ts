b743abe9ada6bd8f7d263530710003af53b1f246
RinNguyen2002 <tuanngocnguyenhuu2002@gmail.com>
2025-06-11 20:34:42 +0700

    Add production environment configuration and remove Strapi Cloud upload settings

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,12 +1,1 @@
-export default () => ({
-  upload: {
-    config: {
-      provider: 'strapi-provider-upload-strapi-cloud',
-      providerOptions: {
-        apiKey: process.env.STRAPI_CLOUD_API_KEY,
-        apiSecret: process.env.STRAPI_CLOUD_API_SECRET,
-        // Các cấu hình khác nếu cần
-      },
-    },
-  },
-});
+export default () => ({});
