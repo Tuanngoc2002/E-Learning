da79692e275e1449ca3a5cf18f27585888178e48
Tuanngoc2002 <nguyenhuutuanngoc00@gmail.com>
2025-06-16 13:59:36 +0700

    Remove authentication and policies for course find and findOne routes

--- backend/src/api/course/routes/course.ts
+++ backend/src/api/course/routes/course.ts
@@ -3,10 +3,12 @@ import { factories } from '@strapi/strapi';
 export default factories.createCoreRouter('api::course.course', {
   config: {
     find: {
-      policies: ['global::allow-public'],
+      auth: false,
+      policies: [],
     },
     findOne: {
-      policies: ['global::allow-public'],
+      auth: false,
+      policies: [],
     },
   },
 });
