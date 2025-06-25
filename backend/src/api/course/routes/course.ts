6f425ad82644f6956ce1b890bc1e9c81de6629d6
Tuanngoc2002 <nguyenhuutuanngoc00@gmail.com>
2025-06-25 08:14:36 +0700

    Update course router: Modify find and findOne endpoints to include 'global::allow-public' policies, enabling public access for course retrieval.

--- backend/src/api/course/routes/course.ts
+++ backend/src/api/course/routes/course.ts
@@ -3,12 +3,10 @@ import { factories } from '@strapi/strapi';
 export default factories.createCoreRouter('api::course.course', {
   config: {
     find: {
-      auth: false,
-      policies: [],
+      policies: ['global::allow-public'],
     },
     findOne: {
-      auth: false,
-      policies: [],
+      policies: ['global::allow-public'],
     },
   },
 });
